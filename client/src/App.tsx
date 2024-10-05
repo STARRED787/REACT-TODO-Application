1f575b3279dbdd849d82db564fdf44348e3892c0
STARRED787 <malithamalshan7@gmail.com>
2024-10-05 13:03:29 +0530

    REACT TODO

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import SignIn from "./components/SignIn";
+
+function App() {
+  return (
+    <>
+      <div className="bg-white">
+        <SignIn />
+      </div>
+    </>
+  );
+}
+
+export default App;
